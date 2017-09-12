2647cc9a83857bce8aadf9e20b7a2937aa99b68a
Vivify Academy <mllemackey@gmail.com>
2017-09-12 21:53:06 +0200

    cors, api/get

--- app/Http/Controllers/GalleryController.php
+++ app/Http/Controllers/GalleryController.php
@@ -10,11 +10,11 @@ class GalleryController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Gallery[]|\Illuminate\Database\Eloquent\Collection|\Illuminate\Http\Response
      */
     public function index()
     {
-        //
+        return Gallery::all();
     }
 
     /**
